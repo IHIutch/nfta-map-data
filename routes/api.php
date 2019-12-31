444e068df6733dede5a42d2d282441e75cbff0c2
IHIutch <jbhutch01@gmail.com>
2019-12-31 13:10:37 -0500

    Get routes by path

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,56 @@ $app->get('/api/all-routes/stops', function ($request, $response) {
     return $response->withHeader('Content-Type', 'application/json');
 });
 
+$app->get('/api/all-routes/paths', function ($request, $response) {
+    $routes = ORM::for_table('routes')
+        ->distinct()->select('route_id')
+        ->select_many('route_long_name')
+        ->order_by_asc('route_id')
+        ->find_many();
+
+    $data = [];
+    foreach ($routes as $route) {
+        $temp_trips = ORM::for_table('trips')
+            ->distinct()->select('shapes.shape_pt_sequence')
+            ->select_many('trips.shape_id', 'shapes.shape_pt_lat', 'shapes.shape_pt_lon')
+            ->join('shapes', [
+                'shapes.shape_id',
+                '=',
+                'trips.shape_id'
+            ])
+            ->where([
+                'trips.route_id' => $route->route_id,
+                'trips.service_id' => 1,
+            ])
+            ->order_by_asc('shape_pt_sequence')
+            ->find_many();
+
+        $temp_shapes = [];
+        foreach ($temp_trips as $trip) {
+            if (!isset($temp_shapes[$trip->shape_id])) {
+                $temp_shapes[$trip->shape_id] = [];
+            };
+            array_push($temp_shapes[$trip->shape_id], [
+                'shape_id' => $trip->shape_id,
+                'shape_pt_sequence' => $trip->shape_pt_sequence,
+                'shape_pt_lat' => $trip->shape_pt_lat,
+                'shape_pt_lon' => $trip->shape_pt_lon
+            ]);
+        };
+
+        array_push($data, [
+            'route_long_name' => $route->route_long_name,
+            'route_id' => $route->route_id,
+            'shapes' => $temp_shapes,
+        ]);
+    };
+
+    $data = json_encode($data);
+
+    $response->getBody()->write(($data));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
 $app->get('/api/route/{route_id}', function ($request, $response, $args) {
     $route_id = $args['route_id'];
 
